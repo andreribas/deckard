2b869a117cb86f143b1b5806b30c097f459bb2e0
André Ribas <andre.ribas@ebanx.com>
2019-10-30 09:22:06 -0300

    Add Utils module and testing support

---
+++ lib/utils.ex
@@ -0,0 +1,19 @@
+defmodule Scrum2000.Utils do
+
+  def is_iso8601_date_between_iso8601_dates?(date, start_date, end_date) do
+    date = date_from_iso8601_datetime(date)
+    start_date = Date.from_iso8601!(start_date)
+    end_date = Date.from_iso8601!(end_date)
+
+    is_date_after_start_date = (Date.compare(date, start_date) !== :lt)
+    is_date_before_end_date = (Date.compare(date, end_date) !== :gt)
+
+    is_date_after_start_date and is_date_before_end_date
+  end
+
+  defp date_from_iso8601_datetime(datetime) do
+    {:ok, datetime, 0} = DateTime.from_iso8601(datetime)
+    DateTime.to_date(datetime)
+  end
+
+end
---
+++ test/utils_test.exs
@@ -0,0 +1,12 @@
+defmodule Scrum2000.UtilsTest do
+  use ExUnit.Case
+
+  alias Scrum2000.Utils
+
+  test "is_iso8601_datetime_between_iso8601_dates" do
+    assert true == Utils.is_iso8601_date_between_iso8601_dates?("2019-09-11T18:30:47Z", "2019-09-10", "2019-09-12")
+    assert false == Utils.is_iso8601_date_between_iso8601_dates?("2019-09-10T18:30:47Z", "2019-09-11", "2019-09-12")
+    assert false == Utils.is_iso8601_date_between_iso8601_dates?("2019-09-12T18:30:47Z", "2019-09-10", "2019-09-11")
+  end
+
+end
